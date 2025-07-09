d0e2b1f967f5f8d5a71a5cc449d7e1386acc8782
LasseMempel <lassemempel@gmail.com>
2025-07-09 20:01:06 +0200

    refactored data flow

--- Mockuppage/src/components/termComponent.vue
+++ Mockuppage/src/components/termComponent.vue
@@ -3,24 +3,53 @@
     <h6 :title="term.definition">
       <a :href="link" target="_blank">{{ term.prefLabel }}</a>
     </h6>
-    <div v-if="term.Feldwert">
-    <UriSelector
-    v-if="term.Feldwert.includes('URI')"
-    :notation="term.notation"
-    @uri-selected="updateFieldValue"
-    />
-    <q-input
-    v-if="term.Feldwert.includes('Text') || term.Feldwert.includes('URI')"
-    v-model="fieldValue"
-    filled
-    dense
-    :readonly="!term.Feldwert.includes('Text')"
-    :hint="term.Verwendungshinweis || ''"
-    @click="handleInputClick"
-    >
-    </q-input>
+    <div v-if="term.Wiederholbar === 'Ja' && term.Feldwert">
+      <div v-for="(value, index) in formManager.formData.value[term.notation]" :key="index" class="row items-center q-mb-sm">
+        <q-input
+          v-model="formManager.formData.value[term.notation][index]"
+          filled
+          dense
+          class="col"
+          :hint="term.Verwendungshinweis || ''"
+        />
+        <q-btn
+          flat
+          round
+          dense
+          icon="delete"
+          color="negative"
+          class="q-ml-sm"
+          @click="formManager.removeField(term.notation, index)"
+          :disable="formManager.formData.value[term.notation].length <= 1"
+        />
+      </div>
+       <q-btn
+          label="Add"
+          icon="add"
+          size="sm"
+          color="positive"
+          @click="formManager.addField(term.notation)"
+        />
     </div>
-    <div v-if="term.narrower">
+
+    <div v-else-if="term.Feldwert">
+      <UriSelector
+        v-if="term.Feldwert.includes('URI')"
+        :notation="term.notation"
+        @uri-selected="(uri) => (formManager.formData.value[term.notation] = uri)"
+      />
+      <q-input
+        v-if="term.Feldwert.includes('Text') || term.Feldwert.includes('URI')"
+        v-model="formManager.formData.value[term.notation]"
+        filled
+        dense
+        :readonly="!term.Feldwert.includes('Text')"
+        :hint="term.Verwendungshinweis || ''"
+        @click="handleInputClick"
+      />
+    </div>
+
+    <div v-if="term.narrower" class="q-ml-md">
       <TermComponent
         v-for="child in term.narrower"
         :key="child.notation"
@@ -31,24 +60,19 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { inject } from 'vue';
 import UriSelector from 'src/components/uriSelector.vue';
 
-const { term } = defineProps(['term'])
-const link = "https://www.w3id.org/conservation/terms/metadata/" + term.notation
+// No local state needed for the input value anymore!
+const props = defineProps(['term']);
+const link = "https://www.w3id.org/conservation/terms/metadata/" + props.term.notation;
 
-// Create a reactive ref to hold the value for the q-input.
-const fieldValue = ref('');
-
-function updateFieldValue(uri) {
-  console.log('Updating field value with URI:', uri);
-  fieldValue.value = uri;
-}
+// Inject the central form manager
+const formManager = inject('formManager');
 
 function handleInputClick() {
-  if (!term.Feldwert.includes('Text')) {
+  if (!props.term.Feldwert.includes('Text')) {
     alert('Please select a value from the normdata URI tree below');
   }
 }
-
 </script>
--- Mockuppage/src/pages/IndexPage.vue
+++ Mockuppage/src/pages/IndexPage.vue
@@ -1,27 +1,78 @@
 <template>
   <q-page>
-    <q-form
-    @submit="onSubmit"
-    @reset="onReset"
-    >
-      <div v-for="term in terms" :key="term.notation">
+    <q-form @submit="onSubmit" @reset="onReset">
+      <div v-for="term in dataDefinition" :key="term.notation">
         <TermComponent :term="term" />
       </div>
       <div>
-        <q-btn label="Submit" type="submit" color="primary"/>
+        <q-btn label="Submit" type="submit" color="primary" />
         <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
       </div>
+      <!-- 
+      <pre>{{ formData }}</pre>
+      -->
     </q-form>
   </q-page>
 </template>
 
 <script setup>
 import { ref, provide } from 'vue';
-import docuData from 'src/data/docu.json';
 import TermComponent from 'src/components/termComponent.vue';
+import docuData from 'src/data/docu.json'; // The static definition
 
+// 1. DATA DEFINITION
+const dataDefinition = ref(docuData);
+
+// 2. FORM STATE
 const formData = ref({});
-const terms = ref(docuData);
 
+// 3. HELPER FUNCTION TO CREATE THE INITIAL FORM STATE
+function createInitialFormData(terms) {
+  const state = {};
+  for (const term of terms) {
+    if (term.Feldwert) { // Only create entries for fields that can have a value
+      if (term.Wiederholbar === 'Ja') { // If repeatable, initialize with an array
+        state[term.notation] = ['']; // Start with one empty input
+      } else {
+        state[term.notation] = '';
+      }
+    }
+    // Recurse for nested terms
+    if (term.narrower) { //
+      Object.assign(state, createInitialFormData(term.narrower));
+    }
+  }
+  return state;
+}
 
+// 4. INITIALIZE AND MANAGE STATE
+formData.value = createInitialFormData(dataDefinition.value);
+
+const onReset = () => {
+  formData.value = createInitialFormData(dataDefinition.value);
+  console.log('Form has been reset!');
+};
+
+const onSubmit = () => {
+  console.log('Final Form Data:', formData.value);
+  // Here you would send formData.value to your backend
+  alert('Form submitted! Check the console for the data.');
+};
+
+// 5. PROVIDE THE FORM MANAGER TO ALL CHILDREN
+provide('formManager', {
+  formData,
+  // Function to add a new instance for a repeatable field
+  addField: (notation) => {
+    if (Array.isArray(formData.value[notation])) {
+      formData.value[notation].push(''); // Add a new empty string to the array
+    }
+  },
+  // Function to remove an instance from a repeatable field
+  removeField: (notation, index) => {
+    if (Array.isArray(formData.value[notation]) && formData.value[notation].length > 1) {
+      formData.value[notation].splice(index, 1); // Remove the item at the given index
+    }
+  }
+});
 </script>
